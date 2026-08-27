b5ca24ee1d3753ca528ec0fc0f407fc886e905c6
Mageto369 <jmnyachio@gmail.com>
2026-08-27 06:39:01 -0500

    feat: place live warehouse intelligence on command center

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { AutonomyConsole } from '@/components/autonomy-console';
 import { CommandCenter } from '@/components/command-center';
+import { LiveWarehousePanel } from '@/components/live-warehouse-panel';
 
 export default function Home() {
   return <>
     <CommandCenter />
+    <LiveWarehousePanel />
     <AutonomyConsole />
   </>;
 }

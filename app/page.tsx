54fd1dfb08d95a1e88f56daf7816f86a1ae98d55
Mageto369 <jmnyachio@gmail.com>
2026-08-27 23:28:25 -0500

    refactor: make command center the functional workspace shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import { ActivationReadiness } from '@/components/activation-readiness';
-import { AgentFleet } from '@/components/agent-fleet';
-import { AutonomyConsole } from '@/components/autonomy-console';
 import { CommandCenter } from '@/components/command-center';
-import { IntelligenceLab } from '@/components/intelligence-lab';
-import { LiveWarehousePanel } from '@/components/live-warehouse-panel';
-import { PromotionGate } from '@/components/promotion-gate';
-import { ShadowPerformance } from '@/components/shadow-performance';
 
 export default function Home() {
-  return <>
-    <CommandCenter />
-    <ActivationReadiness />
-    <ShadowPerformance />
-    <PromotionGate />
-    <IntelligenceLab />
-    <LiveWarehousePanel />
-    <AutonomyConsole />
-    <AgentFleet />
-  </>;
+  return <CommandCenter />;
 }
